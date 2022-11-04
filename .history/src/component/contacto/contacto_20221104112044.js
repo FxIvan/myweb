d39f7b2e65ca8e45fb776d7626244cf7c05e91d0
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2022-11-04 12:07:59 -0300

    agregando formulario

---
+++ .history/src/component/contacto/contacto_20221104112044.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+export const Contacto = () =>{
+    return(
+        <div>
+            <div>
+                <div>
+                    <h2>Contacto</h2>
+                </div>   
+                <div>
+                    <div>
+                        <ul>
+                            <li>Whatsapp: 1136887781</li>
+                            <li>Correo: devealmendra@gmail.com</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <div>
+                            <form>
+                                <label>Correo</label>
+                                <input/>
+                                <label>Telefono</label>
+                                <input/>
+                                <label>Consulta</label>
+                                <input/>
+                                <input type='submit'/>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    )
+}
--- .history/src/component/section1/section1_20221102111836.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Section1 = () =>{
-    return(
-        <div>
-            
-        </div>
-    )
-}
