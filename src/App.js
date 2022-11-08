621fb4dec75424baa70e525a0566e52604b4e334
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2022-11-08 17:15:31 -0300

    agregando browser router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { Routes, Route, HashRouter} from "react-router-dom";
+import { Routes, Route, HashRouter, BrowserRouter} from "react-router-dom";
 import { Header } from "./component/header/header";
 import { IndexPage } from "./component/index-page";
 
 function App() {
   return (
-    <HashRouter>
-    <Routes>
-        <Route path="/" element={<IndexPage/>} />
-    </Routes>
-    </HashRouter>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<IndexPage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
